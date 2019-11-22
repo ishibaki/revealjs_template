1b957c970abbb6cbef31ad87921358a9a03ad354
ishibaki <iroirotomoki@gmail.com>
2019-11-22 17:26:16 +0900

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,6 +3,7 @@
 echo "Enter the title:"
 read title
 sed -i -e "s/^\# Papaer Template/# ${title}/" ./README.md
+sed -i -e "s/^\% Sample/% ${title}/" ./manuscript/0_1_meta.md
 NOW_DATE=`date "+%F"`
 sed -i -e "s/YYYY-MM-DD/${NOW_DATE}/" ./README.md
 git init
@@ -10,4 +11,5 @@ touch manuscript/1_main.md
 mkdir figs
 mkdir out
 rm README.md-e
+rm manuscript/0_1_meta.md-e
 rm init.sh
